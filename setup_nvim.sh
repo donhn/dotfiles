96f5dd21904ad9c4b4ccd95fb1f120477b0997bb
ngdon <ngdon@pm.me>
2023-02-24 11:46:24 -0800

    download latest nvim when running

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,3 +1,6 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb &&
+sudo apt install ./nvim-linux64.deb &&
+rm ./nvim-linux64.deb &&
 git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1 &&
 cp -r .config/nvim $HOME/.config &&
 nvim  
