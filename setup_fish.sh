b42ed9389db0ec2089f0c0e891f9e5f11aef626b
ngdon <ngdon@pm.me>
2023-02-24 11:34:41 -0800

    add fonts, update fish setup

--- setup_fish.sh
+++ setup_fish.sh
@@ -1,7 +1,7 @@
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install fish
-chsh -s $(which fish)
-fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install IlanCosman/tide@v5
+#!/bin/bash
+echo "Installing fish shell."
+sudo apt-add-repository ppa:fish-shell/release-3 &&
+sudo apt update &&
+sudo apt install fish &&
+chsh -s $(which fish) &&
+fish ./setup_fish.1
