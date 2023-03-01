9fecb289208dab2b2a9fa95902d94f518312e7fa
ngdon <ngdon@pm.me>
2023-03-01 11:49:20 -0800

    add ripgrep, npm, pyvenv and unzip

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,3 +1,8 @@
+sudo apt install npm
+sudo apt install python3.10-venv
+sudo apt install ripgrep
+sudo apt install unzip
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb &&
 sudo apt install ./nvim-linux64.deb &&
 rm ./nvim-linux64.deb &&
