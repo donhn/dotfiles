22a9b44d3fa063930b69a51fb77e04bd739bde71
ngdon <ngdon@pm.me>
2023-03-01 12:43:04 -0800

    remove plugins.lua (not used), add rainbow_csv

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -96,4 +96,24 @@ return {
   ["nvim-telescope/telescope-file-browser.nvim"] = {
     cmd = "Telescope",
   },
+
+  -- CSV
+  ["cameron-wags/rainbow_csv.nvim"] = {
+    config = function()
+      require("rainbow_csv").setup()
+    end,
+    module = {
+      "rainbow_csv",
+      "rainbow_csv.fns",
+    },
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+  },
 }
