1143388d3d89b88b5a975cc505b9af386da14f10
donhn <ngdon@pm.me>
2023-09-07 14:46:50 -0700

    rm exa after install

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ elif [ "$OPERATING_SYSTEM" == "Linux" ]; then
     EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
     sudo unzip -d /usr/local/ exa.zip
+    rm exa.zip
     # Meld
     sudo ln -s /mnt/c/Program\ Files/Meld/Meld.exe /usr/local/bin/meld
 fi
