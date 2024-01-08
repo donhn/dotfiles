5e037ac038aeee3192bb74310ff6ec98f7c04a48
Don <ngdon@pm.me>
2024-01-07 19:49:43 -0800

    formatting wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-
-config.default_domain = 'WSL:Ubuntu'
+config.default_domain = "WSL:Ubuntu"
 -- config.default_prog = { 'powershell' }
-
 
 config.window_decorations = "TITLE | RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_opacity = .99
+config.window_background_opacity = 0.99
 -- config.background = {
 --     {
 --         source = { File = 'C:/Users/DonNguyen/Pictures/Backgrounds/exodus.jpg' },
@@ -19,13 +17,10 @@ config.window_background_opacity = .99
 -- }
 config.win32_system_backdrop = "Auto"
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.font = wezterm.font (
-    'MesloLGS NF',
-    {
-        weight = 'Regular'
-    }
-)
+config.color_scheme = "Gruvbox Dark (Gogh)"
+config.font = wezterm.font("MesloLGS NF", {
+	weight = "Regular",
+})
 config.font_size = 10
 
 return config
