3fb3279a271773ed8a83325c00d6363e7c9d4f69
ngdon <ngdon@pm.me>
2024-02-09 18:05:26 -0800

    Update scoop installs

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,15 +1,8 @@
 winget install --id Microsoft.Powershell --source winget
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-scoop install main/neovim
-scoop install main/nodejs
-scoop install main/python
-scoop install main/gcc
-scoop install main/git
-scoop install ninja
-scoop install cmake
-
-scoop bucket add nerd-fonts
-scoop install nerd-fonts/Meslo-NF
+scoop bucket add nerd-fonts extras
+scoop install 7zip clangd cmake delta fd ffmpeg gcc lazygit mingw neovim \
+ninja nodejs perl python ripgrep wezterm Meslo-NF
 
 git clone https://github.com/NvChad/NvChad $HOME\AppData\Local\nvim --depth 1
