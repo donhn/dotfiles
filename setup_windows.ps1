e8f565e76a984cda9ce02e9defada4b8f431cb43
ngdon <ngdon@pm.me>
2024-02-11 19:13:39 -0800

    Fix scoop install

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,9 +1,10 @@
 winget install --id Microsoft.Powershell --source winget
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop install git
 scoop bucket add nerd-fonts 
 scoop bucket add extras
-scoop install 7zip clangd cmake delta fd ffmpeg firefox gcc lazygit Meslo-NF`
+scoop install 7zip clangd cmake delta fd ffmpeg firefox gcc lazygit Meslo-NF `
 mingw neovim nextcloud ninja nodejs obsidian perl python ripgrep sharpkeys `
 spotify vscode wezterm winscp
 git clone https://github.com/NvChad/NvChad $HOME\AppData\Local\nvim --depth 1
